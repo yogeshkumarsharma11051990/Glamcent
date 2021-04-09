78a606db242769cb909a266078b05609c9401ee6
yogesh.s@sarvang.com <yogesh.s@sarvang.com>
2021-04-09 20:07:40 +0530

    234324

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ try{
     $API_KEY = '47190794';
     $API_SECRET = '749b45cf5576d90b0f1e76a0d0ac3097a07f5789';
 
-    echo "1";
+    //echo "1";
     //die();
 
     $apiObj = new OpenTok($API_KEY, $API_SECRET);

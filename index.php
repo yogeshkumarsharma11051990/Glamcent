40abeec4bac317a0dc9ec8abebc7a78e4424c1e9
yogesh.s@sarvang.com <yogesh.s@sarvang.com>
2021-04-09 19:55:23 +0530

    1

--- index.php
+++ index.php
@@ -5,4 +5,22 @@ include('vendor/autoload.php');
 
 echo "hello";
 
+try{
+
+    $API_KEY = '47190794';
+    $API_SECRET = '749b45cf5576d90b0f1e76a0d0ac3097a07f5789';
+
+    echo "1";
+    die();
+
+    $apiObj = new OpenTok($API_KEY, $API_SECRET);
+    $session = $apiObj->createSession(array('mediaMode' => MediaMode::ROUTED));
+    echo $session->getSessionId();
+    
+}catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+  }
+
+
+
 ?>

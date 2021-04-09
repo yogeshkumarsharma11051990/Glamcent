10bdb480fc622ecfbe58402f4219f8117d092489
yogesh.s@sarvang.com <yogesh.s@sarvang.com>
2021-04-09 21:48:59 +0530

    354324ewewr

--- generate_token.php
+++ generate_token.php
@@ -6,25 +6,27 @@ use OpenTok\Session;
 use OpenTok\Role; 
 use OpenTok\MediaMode;
 
-//$sessionId = $_REQUEST['session_id'];
-
 $API_KEY = '47190794';
 $API_SECRET = '749b45cf5576d90b0f1e76a0d0ac3097a07f5789';
 $apiObj = new OpenTok($API_KEY, $API_SECRET);
 
 $sessionMain = $apiObj->createSession(array('mediaMode' => MediaMode::ROUTED));
 
-$token = $apiObj->generateToken($sessionMain->getSessionId());
+$sessionId = $sessionMain->getSessionId();
+
+$token = $apiObj->generateToken($sessionId);
 // Generate a Token by calling the method on the Session (returned from createSession)
-$token = $session->generateToken();
+$token = $sessionMain->generateToken();
 
 // Set some options in a token
-echo  $token = $session->generateToken(array(
+$token = $sessionMain->generateToken(array(
     'role'       => Role::MODERATOR,
     'expireTime' => time()+(7 * 24 * 60 * 60), // in one week
     'data'       => 'name=Johnny',
     'initialLayoutClassList' => array('focus')
 ));
+
+echo "SessionId : ".$sessionId."/nTokenId".$token;
 
 
 ?>
